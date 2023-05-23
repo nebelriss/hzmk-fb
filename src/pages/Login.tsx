42bec4e42d55a403120077865af36c8d641e63f3
Michel Heiniger <git@heiniger.io>
2023-05-23 20:34:33 +0200

    Removed unnecessary comment

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -31,14 +31,6 @@ export const LoginPage = () => {
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full bg-gray-50">
-        <body class="h-full">
-        ```
-      */}
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
